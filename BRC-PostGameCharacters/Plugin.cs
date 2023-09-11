eb51d7b25ea6f6e3141ea8bba0bb39fa7b882180
Michael <15317421+ActualMandM@users.noreply.github.com>
2023-09-10 18:46:26 -0700

    Allow jetpackless Faux to be added to Cypher via config.

--- BRC-PostGameCharacters/Plugin.cs
+++ BRC-PostGameCharacters/Plugin.cs
@@ -1,17 +1,22 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
 using Reptile;
 
 namespace BRC_PostGameCharacters
 {
-	[BepInPlugin("com.MandM.BRC-PostGameCharacters", "BRC-PostGameCharacters", "1.0.1")]
+	[BepInPlugin("com.MandM.BRC-PostGameCharacters", "BRC-PostGameCharacters", "1.0.2")]
 	[BepInProcess("Bomb Rush Cyberfunk.exe")]
 
 	public class Plugin : BaseUnityPlugin
 	{
-		public void Awake()
+        public static ConfigEntry<bool> JetpacklessFaux = null!;
+
+        public void Awake()
 		{
-			Harmony harmony = new Harmony("MandM.BRC-PostGameCharacters.Harmony");
+            JetpacklessFaux = Config.Bind("General", "JetpacklessFaux", false, "Adds jetpackless Faux to the Cypher.");
+
+            Harmony harmony = new Harmony("MandM.BRC-PostGameCharacters.Harmony");
 			harmony.PatchAll();
 		}
 	}
@@ -28,6 +33,7 @@ namespace BRC_PostGameCharacters
                 if ((int)player.character != 12) __instance.selectableCharacters.Add(Characters.headMan);
                 if ((int)player.character != 25) __instance.selectableCharacters.Add(Characters.legendMetalHead);
                 if ((int)player.character != 24) __instance.selectableCharacters.Add(Characters.eightBallBoss);
+                if (Plugin.JetpacklessFaux.Value && (int)player.character != 23) __instance.selectableCharacters.Add(Characters.headManNoJetpack);
 
                 __instance.Shuffle(__instance.selectableCharacters);
             }

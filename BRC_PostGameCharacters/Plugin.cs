159e665a309743d133c1e74d9f7bc32b39aa6ab5
Michael <15317421+ActualMandM@users.noreply.github.com>
2024-03-15 19:12:23 -0700

    Code refactor.

--- BRC_PostGameCharacters/Plugin.cs
+++ BRC_PostGameCharacters/Plugin.cs
@@ -5,18 +5,18 @@ using Reptile;
 
 namespace BRC_PostGameCharacters
 {
-    [BepInPlugin("com.MandM.BRC-PostGameCharacters", "BRC-PostGameCharacters", "1.0.4")]
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     [BepInProcess("Bomb Rush Cyberfunk.exe")]
 
     public class Plugin : BaseUnityPlugin
     {
-        public static ConfigEntry<bool> JetpacklessFaux = null!;
+        public static ConfigEntry<bool> BoostpacklessFaux = null!;
 
         public void Awake()
         {
-            JetpacklessFaux = Config.Bind("General", "JetpacklessFaux", false, "Adds jetpackless Faux to the Cypher.");
+            BoostpacklessFaux = Config.Bind("General", "Boostpackless Faux", false, "Adds boostpackless Faux to the Cypher.");
 
-            Harmony harmony = new Harmony("MandM.BRC-PostGameCharacters.Harmony");
+            Harmony harmony = new(PluginInfo.PLUGIN_GUID);
             harmony.PatchAll();
         }
     }
@@ -30,16 +30,20 @@ namespace BRC_PostGameCharacters
         {
             if (Story.GetCurrentObjectiveInfo().chapter == Story.Chapter.CHAPTER_6)
             {
+                // Faux
                 if ((int)player.character != 12)
                     __instance.selectableCharacters.Add(Characters.headMan);
 
+                // Red Felix
                 if ((int)player.character != 25 && !__instance.selectableCharacters.Contains((Characters)25))
                     __instance.selectableCharacters.Add(Characters.legendMetalHead);
 
+                // DOT.EXE (Boss)
                 if ((int)player.character != 24)
                     __instance.selectableCharacters.Add(Characters.eightBallBoss);
 
-                if (Plugin.JetpacklessFaux.Value && (int)player.character != 23)
+                // Faux (boostpackless)
+                if (Plugin.BoostpacklessFaux.Value && (int)player.character != 23)
                     __instance.selectableCharacters.Add(Characters.headManNoJetpack);
 
                 __instance.Shuffle(__instance.selectableCharacters);
